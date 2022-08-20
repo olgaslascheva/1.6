5194c800a372679630acfbce00fbe4795661adbd
olgaslascheva <andreeva.olga98@mail.ru>
2022-08-20 10:14:58 +0400

    3

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,37 @@ import './menu.js';
 
 import Swiper, { Pagination } from 'swiper';
 
-const swiper = new Swiper('.swiper', {
-  modules: [ Pagination],
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  slidesPerView: 1.25,
-});
+// const swiper = new Swiper('.swiper', {
+//   modules: [ Pagination],
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+//   slidesPerView: 1.25,
+// });
+
+let swipers = document.querySelector('.swiper');
+let mobileSwiper;
+
+function initMobileSwiper() {
+  if (window.innerWidth <= 600 && swipers.dataset.mobile == 'false') {
+    mobileSwiper = new Swiper (swipers, {
+      modules: [ Pagination],
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      slidesPerView: 1.25,
+    });
+
+    swipers.dataset.mobile = 'true';
+  }
+
+  if (window.innerWidth > 600) {
+    swipers.dataset.mobile = 'false';
+
+    mobileSwiper.destroy();
+  }
+}
+
+initMobileSwiper();
