c05c732f13e31be2e7c4942273e5d7b0f00a9eae
olgaslascheva <andreeva.olga98@mail.ru>
2022-08-20 09:39:00 +0400

    3

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
 import '../scss/style.scss';
 
 import './menu.js';
+
+import Swiper, { Pagination } from 'swiper';
+
+const swiper = new Swiper('.swiper', {
+  modules: [ Pagination],
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  slidesPerView: 1.25,
+});
